51cbbfc1d4d93a62f6677d732e6367d8352751dc
uraniumnutt <ethummel5@gmail.com>
2023-03-23 17:48:41 -0500

    Finished Question3

--- Assignment2/Question 3/Question 3.srcs/sim_1/new/Testbench.v
+++ Assignment2/Question 3/Question 3.srcs/sim_1/new/Testbench.v
@@ -1,26 +1,55 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/23/2023 12:46:35 PM
-// Design Name: 
-// Module Name: Testbench
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
-module Testbench(
 
-    );
+module Testbench;
+
+        reg [1:0] d;
+        reg clk;
+        reg reset;
+        reg [1:0] op;
+        wire [1:0] count;
+
+        TwoBitStateCount uut(
+
+            .d(d),
+            .clk(clk),
+            .reset(reset),
+            .op(op),
+            .count(count)
+
+        );
+
+        always
+            #5 clk = ~clk;
+
+        initial begin
+            
+            // init the clk
+            clk = 0; reset = 0;
+            // load 2 into the counter
+            d = 2'd2; op = 2'b11; #10
+            // count down
+            op = 2'b10; #10
+            // count up
+            op = 2'b01; #10
+            // count up again
+            op = 2'b01; #10
+            // count up one more time
+            op = 2'b01; #10
+            // do nothing
+            op = 2'b00; #10
+            // load 1 into the counter
+            d = 2'd1; op = 2'b11; #10
+            // count up (overflow)
+            op = 2'b01; #10
+            // count down (underflow)
+            op = 2'b10; #10
+            // do nothing for the last cycle
+            op = 2'b00;
+
+
+        end
+
+
 endmodule
--- Assignment2/Question 3/Question 3.srcs/sources_1/new/TwoBitStateCount.v
+++ Assignment2/Question 3/Question 3.srcs/sources_1/new/TwoBitStateCount.v
@@ -1,25 +1,98 @@
 module TwoBitStateCount(
 
+    input [1:0] d,
     input clk,
     input reset,
-    output reg count
+    input [1:0] op,
+    output reg [1:0] count
 
     );
+
+    localparam zero = 2'b00;
+    localparam one = 2'b01;
+    localparam two = 2'b10;
+    localparam three = 2'b11;
 
     reg [1:0] currentState;
     reg [1:0] nextState;
 
-    always @(posedge clk) begin
-        
-        currentState = nextState;
+    initial begin
+
+        currentState = zero;
+        count = zero;
 
     end
 
-    always @(reset) begin
+    always @(posedge clk) begin
+        
+        currentState = nextState;
+        count = currentState;
 
-        if (reset == 1)
-            nextState = 0;
-        else
+    end
+
+    always @(reset or d or op or currentState) begin
+
+        case (currentState)
+
+            zero: begin
+                if (reset == 1)
+                    nextState = zero;
+
+                case (op) 
+
+                    0: nextState = zero;
+                    1: nextState = one;
+                    2: nextState = three;
+                    3: nextState = d;
+
+                endcase
+                
+            end
+
+            one: begin
+                if (reset == 1)
+                    nextState = zero;
+
+                    case (op) 
+
+                    0: nextState = one;
+                    1: nextState = two;
+                    2: nextState = zero;
+                    3: nextState = d;
+
+                endcase
+            end
+
+            two: begin
+                if (reset == 1)
+                    nextState = zero;
+
+                case (op) 
+
+                    0: nextState = two;
+                    1: nextState = three;
+                    2: nextState = one;
+                    3: nextState = d;
+
+                endcase
+            end
+
+            three: begin
+                if (reset == 1)
+                    nextState = zero;
+
+                    case (op) 
+
+                    0: nextState = three;
+                    1: nextState = zero;
+                    2: nextState = two;
+                    3: nextState = d;
+
+                endcase
+            end
+
+
+        endcase
             
 
     end
